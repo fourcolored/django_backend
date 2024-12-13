0204483c7b7d98fd48c7f764edee22f3ea384d25
moonidelight <moonblood.delight@gmail.com>
2024-12-14 04:43:17 +0500

    upd

--- core/core/router.py
+++ core/core/router.py
@@ -4,10 +4,9 @@ from django.conf import settings
 class PrimaryReplicaRouter:
     def db_for_read(self, model, **hints):
         """ Reads go to a randomly-chosen replica. """
-        # return random.choice(['default', 'replica'])
         if settings.TESTING:
             return 'default'
-        return 'replica'
+        return random.choice(['default', 'replica'])
     
     def db_for_write(self, model, **hints):
         """ Writes always go to primary. """
